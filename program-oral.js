70c1a430d55219565a1060dd336632c97ca72e8e
Silvana Ovaitt <silvana.ayala@nrel.gov>
2024-03-05 10:16:04 -0700

    update times

--- program-oral.js
+++ program-oral.js
@@ -1,5 +1,235 @@
 var program = [
   {
+    "id": "Tue",
+    "label": "Tue 5 Mar",
+    "color": "#008080",
+    "sessions": [
+      {
+        "label": "",
+        "presentations": [
+          {
+            "badge": "7:30 AM",
+            "presentation": "Breakfast",
+            "duramat": false
+          }
+        ]
+      },
+      {
+        "label": "",
+        "presentations": [
+          {
+            "badge": "8:30 AM",
+            "presentation": "Welcome Day 2",
+            "duramat": false
+          }
+        ]
+      },
+      {
+        "label": "IV. Corporate value from circularity ",
+        "chairs": "Lizzy Aldridge, SOLARCYCLE",
+        "presentations": [
+          {
+            "badge": "8:45 AM",
+            "presentation": "Vidha Dixit",
+            "author": "EDP",
+            "duramat": false
+          },
+          {
+            "presentation": "Kelly Weger",
+            "author": "Q Cells",
+            "duramat": false
+          },
+          {
+            "presentation": "Nick de Vries",
+            "author": "Silicon Ranch",
+            "duramat": false
+          },
+        ]
+      },
+      {
+        "label": "",
+        "presentations": [
+          {
+            "badge": "9:45 AM",
+            "presentation": "Coffee Break",
+          }
+        ]
+      },
+      {
+        "label": "",
+        "presentations": [
+          {
+            "badge": "10:40 AM",
+            "presentation": "Magdi Azer, CTO, Remade Institute",
+            "author": "Title TBC",
+            "duramat": false
+          }
+        ]
+      },
+      {
+        "label": "",
+        "presentations": [
+          {
+            "badge": "11:10 AM",
+            "presentation": "The Element of Surprise: How PV Module Materials May Affect Decision-Making and Recycling/Upcycling Potential",
+            "author": "Cara Libby, EPRI",
+            "duramat": false
+          }
+        ]
+      },
+      {
+        "label": "",
+        "presentations": [
+          {
+            "badge": "12:00 PM",
+            "presentation": "Buffet Lunch",
+            "duramat": false
+          }
+        ]
+      },
+      {
+        "label": "V. Standards as catalysts for driving circularity",
+        "chairs": "Karen Drozdiak, First Solar",
+        "presentations": [
+          {
+            "badge": "1:00 PM",
+            "presentation": "Mike Easterbrook",
+            "author": "SERI",
+            "duramat": false
+          },
+          {
+            "presentation": "Evelyn Butler",
+            "author": "Solar Energy Industries Association",
+            "duramat": false
+          },
+          {
+            "presentation": "Rich Stromberg",
+            "author": "Equitable Solar Solutions/Univ of Alaska Fairbanks",
+            "duramat": false
+          },
+          {
+            "presentation": "Mar\u00eda de las Nieves Espinosa Mart\u00ednez",
+            "author": "Univercidad de Murcia",
+            "duramat": false
+          }
+        ]
+      },
+
+      {
+        "label": "Coffee Break & Poster Session",
+        "presentations": [
+          {
+            "badge": "2:30 PM",
+            "presentation": "See the poster listing here",
+            "url": "program-posters-A.html",
+            "duramat": false
+          }
+        ]
+      },
+      {
+        "label": "BREAK OUT SESSION",
+        "presentations": [
+          {
+            "badge": "3:10 PM",
+          }]
+      },
+      {
+        "label": "",
+        "presentations": [
+          {
+            "badge": "4:30 PM",
+            "presentation": "Adjourn",
+            "duramat": false
+          }
+        ]
+      },
+      {
+        "label": "",
+        "presentations": [
+          {
+            "badge": "6:00 PM",
+            "presentation": "No host dinner at Zeppelin Station",
+            "author": "3501 Wazee St., Ste 100, Denver CO",
+            "duramat": false
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": "Wed",
+    "label": "Wed 6 Mar",
+    "color": "#6baed6",
+    "sessions": [
+      {
+        "label": "",
+        "presentations": [
+          {
+            "badge": "8:30 AM",
+            "presentation": "Shuttle departs from Sheraton Hotel, Lakewood to NREL",
+            "duramat": false
+          }
+        ]
+      },
+      {
+        "label": "NREL Tours",
+        "badge": "9:00 AM",
+
+      },
+      {
+        "label": "",
+        "presentations": [
+          {
+            "badge": "11:15 AM",
+            "presentation": "Shuttle departs to Sheraton Hotel, Lakewood to NREL",
+            "duramat": false
+          }
+        ]
+      },
+    ]
+  },
+  {
+    "id": "Sun (PAST)",
+    "label": "Sun 3 Mar (PAST)",
+    "color": "#00FFFF",
+    "sessions": [
+      {
+        "label": "",
+        "presentations": [
+          {
+            "badge": "9:00 AM",
+            "presentation": "Tutorial AM: Sustainable Solar Futures: Mastering LCA, Criticality Metrics, and Material Flow Analysis in Photovoltaics",
+            "author": "Dr. Annick Anctil, Dr. Preeti Nain, Luyao Yuan",
+            "affiliation": "Michigan State University",
+            "duramat": false
+          }
+        ]
+      },
+      {
+        "label": "",
+        "presentations": [
+          {
+            "badge": "12:00 PM",
+            "presentation": "Lunch",
+            "duramat": false
+          }
+        ]
+      },
+      {
+        "label": "",
+        "presentations": [
+          {
+            "badge": "1:00 PM",
+            "presentation": "Tutorial PM: Sustainability in the Solar Energy Transition: an interactive tutorial for evaluating mass and energy metrics",
+            "author": "Dr. Silvana Ovaitt, Dr. Heather Mirletz, Dr. Rachel Woods-Robinson",
+            "affiliation": "NREL",
+            "duramat": false
+          }
+        ]
+      }
+    ]
+  },
+  {
     "id": "Mon",
     "label": "Mon 4 Mar",
     "color": "#40E0D0",
@@ -173,236 +403,6 @@ var program = [
             "badge": "6:00 PM",
             "presentation": "No host social hour at Edgewater Market Place",
             "author": "5505 W 20th Ave, Edgewater, CO 80214",
-            "duramat": false
-          }
-        ]
-      }
-    ]
-  },
-  {
-    "id": "Tue",
-    "label": "Tue 5 Mar",
-    "color": "#008080",
-    "sessions": [
-      {
-        "label": "",
-        "presentations": [
-          {
-            "badge": "7:30 AM",
-            "presentation": "Breakfast",
-            "duramat": false
-          }
-        ]
-      },
-      {
-        "label": "",
-        "presentations": [
-          {
-            "badge": "8:30 AM",
-            "presentation": "Welcome Day 2",
-            "duramat": false
-          }
-        ]
-      },
-      {
-        "label": "IV. Corporate value from circularity ",
-        "chairs": "Lizzy Aldridge, SOLARCYCLE",
-        "presentations": [
-          {
-            "badge": "8:45 AM",
-            "presentation": "Vidha Dixit",
-            "author": "EDP",
-            "duramat": false
-          },
-          {
-            "presentation": "Kelly Weger",
-            "author": "Q Cells",
-            "duramat": false
-          },
-          {
-            "presentation": "Nick de Vries",
-            "author": "Silicon Ranch",
-            "duramat": false
-          },
-        ]
-      },
-      {
-        "label": "",
-        "presentations": [
-          {
-            "badge": "9:45 AM",
-            "presentation": "Coffee Break",
-          }
-        ]
-      },
-      {
-        "label": "",
-        "presentations": [
-          {
-            "badge": "10:30 AM",
-            "presentation": "Magdi Azer, CTO, Remade Institute",
-            "author": "Title TBC",
-            "duramat": false
-          }
-        ]
-      },
-      {
-        "label": "",
-        "presentations": [
-          {
-            "badge": "11:00 AM",
-            "presentation": "The Element of Surprise: How PV Module Materials May Affect Decision-Making and Recycling/Upcycling Potential",
-            "author": "Cara Libby, EPRI",
-            "duramat": false
-          }
-        ]
-      },
-      {
-        "label": "",
-        "presentations": [
-          {
-            "badge": "12:00 PM",
-            "presentation": "Buffet Lunch",
-            "duramat": false
-          }
-        ]
-      },
-      {
-        "label": "V. Standards as catalysts for driving circularity",
-        "chairs": "Karen Drozdiak, First Solar",
-        "presentations": [
-          {
-            "badge": "1:00 PM",
-            "presentation": "Mike Easterbrook",
-            "author": "SERI",
-            "duramat": false
-          },
-          {
-            "presentation": "Evelyn Butler",
-            "author": "Solar Energy Industries Association",
-            "duramat": false
-          },
-          {
-            "presentation": "Rich Stromberg",
-            "author": "Equitable Solar Solutions/Univ of Alaska Fairbanks",
-            "duramat": false
-          },
-          {
-            "presentation": "Mar\u00eda de las Nieves Espinosa Mart\u00ednez",
-            "author": "Univercidad de Murcia",
-            "duramat": false
-          }
-        ]
-      },
-
-      {
-        "label": "Coffee Break & Poster Session",
-        "presentations": [
-          {
-            "badge": "2:30 PM",
-            "presentation": "See the poster listing here",
-            "url": "program-posters-A.html",
-            "duramat": false
-          }
-        ]
-      },
-      {
-        "label": "BREAK OUT SESSION",
-        "presentations": [
-          {
-            "badge": "3:10 PM",
-          }]
-      },
-      {
-        "label": "",
-        "presentations": [
-          {
-            "badge": "4:30 PM",
-            "presentation": "Adjourn",
-            "duramat": false
-          }
-        ]
-      },
-      {
-        "label": "",
-        "presentations": [
-          {
-            "badge": "6:00 PM",
-            "presentation": "No host dinner at Zeppelin Station",
-            "author": "3501 Wazee St., Ste 100, Denver CO",
-            "duramat": false
-          }
-        ]
-      }
-    ]
-  },
-  {
-    "id": "Wed",
-    "label": "Wed 6 Mar",
-    "color": "#6baed6",
-    "sessions": [
-      {
-        "label": "",
-        "presentations": [
-          {
-            "badge": "8:30 AM",
-            "presentation": "Shuttle departs from Sheraton Hotel, Lakewood to NREL",
-            "duramat": false
-          }
-        ]
-      },
-      {
-        "label": "NREL Tours",
-        "badge": "9:00 AM",
-
-      },
-      {
-        "label": "",
-        "presentations": [
-          {
-            "badge": "11:15 AM",
-            "presentation": "Shuttle departs to Sheraton Hotel, Lakewood to NREL",
-            "duramat": false
-          }
-        ]
-      },
-    ]
-  },
-  {
-    "id": "Sun (PAST)",
-    "label": "Sun 3 Mar (PAST)",
-    "color": "#00FFFF",
-    "sessions": [
-      {
-        "label": "",
-        "presentations": [
-          {
-            "badge": "9:00 AM",
-            "presentation": "Tutorial AM: Sustainable Solar Futures: Mastering LCA, Criticality Metrics, and Material Flow Analysis in Photovoltaics",
-            "author": "Dr. Annick Anctil, Dr. Preeti Nain, Luyao Yuan",
-            "affiliation": "Michigan State University",
-            "duramat": false
-          }
-        ]
-      },
-      {
-        "label": "",
-        "presentations": [
-          {
-            "badge": "12:00 PM",
-            "presentation": "Lunch",
-            "duramat": false
-          }
-        ]
-      },
-      {
-        "label": "",
-        "presentations": [
-          {
-            "badge": "1:00 PM",
-            "presentation": "Tutorial PM: Sustainability in the Solar Energy Transition: an interactive tutorial for evaluating mass and energy metrics",
-            "author": "Dr. Silvana Ovaitt, Dr. Heather Mirletz, Dr. Rachel Woods-Robinson",
-            "affiliation": "NREL",
             "duramat": false
           }
         ]
